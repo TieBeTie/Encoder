96169498c08f17f05f974fdc65e14fed77921877
Jerry Treeman <ramafreemen@gmail.com>
2020-05-10 20:59:29 +0300

    code update

--- encryptor.py
+++ encryptor.py
@@ -1,14 +1,14 @@
 import argparse
-import sys
 import json
 import string
+import sys
 
 parser = argparse.ArgumentParser(description='Encoder')
 subparsers = parser.add_subparsers()
 
 parser_encode = subparsers.add_parser('encode', help='Шифратор')
 parser_encode.set_defaults(mode='encode')
-parser_encode.add_argument('--cipher', choices=['caesar', 'vignere'],
+parser_encode.add_argument('--cipher', choices=['caesar', 'vigenere'],
                            required=True, help='Введите шифр')
 parser_encode.add_argument('--key', help='Ключ шифра', required=True)
 parser_encode.add_argument('--input-file', help='Путь к входному файлу')
@@ -16,7 +16,7 @@ parser_encode.add_argument('--output-file', help='Путь к выходному
 
 parser_decode = subparsers.add_parser('decode', help='Расшифратор')
 parser_decode.set_defaults(mode='decode')
-parser_decode.add_argument('--cipher', choices=['caesar', 'vignere'],
+parser_decode.add_argument('--cipher', choices=['caesar', 'vigenere'],
                            required=True, help='Введите шифр')
 parser_decode.add_argument('--key', help='Ключ шифра', required=True)
 parser_decode.add_argument('--input-file', help='Путь к входному файлу')
@@ -24,15 +24,15 @@ parser_decode.add_argument('--output-file', help='Путь к выходному
 
 parser_train = subparsers.add_parser('train', help='Искусственное обучение')
 parser_train.set_defaults(mode='train')
-parser_train.add_argument('--cipher', choices=['caesar', 'vignere'],
+parser_train.add_argument('--cipher', choices=['caesar', 'vigenere'],
                           required=True, help='Введите шифр')
 parser_train.add_argument('--key', help='Ключ шифра', required=True)
 parser_train.add_argument('--input-file', help='Путь к входному файлу')
-parser_train.add_argument('--output-file', help='Путь к выходному файлу')
+parser_train.add_argument('--output-file', help='Путь к выfходному файлу')
 
 parser_hack = subparsers.add_parser('hack', help='Взлом by Vladislav Hacker')
 parser_hack.set_defaults(mode='hack')
-parser_hack.add_argument('--cipher', choices=['caesar', 'vignere'],
+parser_hack.add_argument('--cipher', choices=['caesar', 'vigenere'],
                          required=True, help='Введите шифр')
 parser_hack.add_argument('--input-file', help='Путь к входному файлу')
 parser_hack.add_argument('--output-file', help='Путь к выходному файлу')
@@ -40,70 +40,77 @@ parser_hack.add_argument('--model-file', help='Модель обучения', r
 
 args = parser.parse_args()
 
+lowercase_eng = string.ascii_lowercase
+uppercase_eng = string.ascii_uppercase
 
-def caeser(input_text, key_=0):
-    result = ""
-    key_ = int(key_)
-    lowercase_eng = string.ascii_lowercase
-    uppercase_eng = string.ascii_uppercase
 
+def ind_lowercase(letter):
+    for sym in range(lowercase_eng):
+        if letter == lowercase_eng[sym]:
+            return sym
+
+
+def ind_uppercase(letter):
+    for sym in range(uppercase_eng):
+        if letter == uppercase_eng[sym]:
+            return sym
+
+
+def shift_letter(letter, key):
+    if letter.islower:
+        return lowercase_eng[(ind_lowercase(letter) + key) %
+                             len(lowercase_eng)]
+    elif letter.isupper:
+        return uppercase_eng[(ind_uppercase(letter) + key) %
+                             len(uppercase_eng)]
+    return letter
+
+
+def reverse_letter(letter):
+    if letter.islower:
+        return lowercase_eng[len(lowercase_eng) - (ind_lowercase(letter))]
+    elif letter.isupper:
+        return uppercase_eng[len(uppercase_eng) - (ind_uppercase(letter))]
+    return letter
+
+
+def caesar(input_text, key=0):
+    key = int(key)
+    result = []
     for sym in input_text:
-        if sym.islower():
-            result += lowercase_eng[(ord(sym) - ord(lowercase_eng[0]) +
-                                    key_) % len(lowercase_eng)]
-        elif sym.isupper():
-            result += uppercase_eng[(ord(sym) - ord(uppercase_eng[0]) +
-                                    key_) % len(uppercase_eng)]
-        else:
-            result += sym
-    return result
+        result.apppend(shift_letter(sym, key))
+    return ''.join(result)
 
 
 def make_key_reverse(input_key=''):
-    temp_key = input_key
-    reversed_key = ''
-    lowercase_eng = string.ascii_lowercase
-    uppercase_eng = string.ascii_uppercase
-
-    for sym in temp_key:
-        tmp_ord = ord(sym) - ord(lowercase_eng[0])
-        if sym.islower():
-            reversed_key += lowercase_eng[len(lowercase_eng) -
-                                          (ord(sym) - ord(lowercase_eng[0]))]
-        if sym.isupper():
-            reversed_key += uppercase_eng[len(uppercase_eng) -
-                                          (ord(sym) - ord(uppercase_eng[0]))]
-    return reversed_key
+    result = []
+    for sym in input_key:
+        result.apppend(reverse_letter(sym))
+    return ''.join(result)
 
 
-def vignere(input_text, encrypt_key='', encrypt_type='encode'):
+def vigenere(input_text, encrypt_key='', encrypt_type='encode'):
     if encrypt_type == 'decode':
         encrypt_key = make_key_reverse(encrypt_key)
 
-    result = ''
-    temp_key = encrypt_key.lower()
-    key_num = 0
-    lowercase_eng = string.ascii_lowercase
-    uppercase_eng = string.ascii_uppercase
+    result = []
+    iteration = 0
 
     for sym in input_text:
         if sym.islower():
-            tmp_order = ord(sym) - ord(lowercase_eng[0])
-            key_order = ord(temp_key[key_num]) - ord(lowercase_eng[0])
-            result += lowercase_eng[(tmp_order + key_order) %
-                                    len(lowercase_eng)]
-            key_num += 1
+            key_order = ind_lowercase(encrypt_key[iteration].lower())
+            result.apppend(shift_letter(sym, key_order))
+            iteration += 1
         elif sym.isupper():
-            tmp_order = ord(sym) - ord(uppercase_eng[0])
-            key_order = ord(temp_key[key_num].upper()) - ord(uppercase_eng[0])
-            result += uppercase_eng[(tmp_order + key_order) %
-                                    len(uppercase_eng)]
-            key_num += 1
+            key_order = ind_uppercase(encrypt_key[iteration].upper())
+            result.apppend(shift_letter(sym, key_order))
+            iteration += 1
         else:
-            result += sym
-        if key_num == len(encrypt_key):
-            key_num = 0
-    return result
+            result.apppend(sym)
+
+        if iteration == len(encrypt_key):
+            iteration = 0
+    return ''.join(result)
 
 
 def letter_nums(word):
@@ -115,11 +122,11 @@ def letter_nums(word):
 
 
 def count_stat(word):
-    alphabet = string.ascii_lowercase
+    alphabet = lowercase_eng
     result = {}
     temp_word = word.lower()
     letter_size = letter_nums(temp_word)
-    for letter in alphabet:
+    for letter in lowercase_eng:
         if letter in temp_word:
             result[letter] = temp_word.count(letter) / letter_size
         else:
@@ -143,58 +150,59 @@ def minus_stats(stats_1, stats_2):
 
 
 def hack_caesar(encoded_text, default_stats_):
-    best_index = 27
     best_stats = None
+    best_index = len(lowercase_eng + 1)
 
-    for i in range(1, 27):
-        stats_tmp = count_stat(caeser(str(encoded_text), i))
+    for i in range(1, best_index):
+        stats_tmp = count_stat(caesar(encoded_text, i))
         difference = minus_stats(default_stats_, stats_tmp)
         if difference < best_stats:
             best_stats = difference
             best_index = i
 
-    result = caeser(encoded_text, best_index)
+    result = caesar(encoded_text, best_index)
     return result
 
 
-input_string = ''
-output_string = ''
+def main():
+    input_string = ''
+    output_string = ''
 
-if args.mode == 'encode' or args.mode == 'decode' or args.mode == 'hack':
-    if args.input_file:
-        input_file = open(args.input_file, 'r')
-        input_string = input_file.read()
-        input_file.close()
+    if args.mode == 'encode' or args.mode == 'decode' or args.mode == 'hack':
+        if args.input_file:
+            input_file = open(args.input_file, 'r')
+            input_string = input_file.read()
+            input_file.close()
+        else:
+            input_string = sys.stdin.read()
+
+    if args.cipher == "caesar":
+        key = int(args.key)
+        if args.mode == "decode":
+            key = -key
+        output_string = caesar(input_string, key)
+    if args.cipher == "vigenere":
+        output_string = vigenere(input_string, args.key, args.mode)
+
+    if args.mode == "train":
+        text = ''
+        if args.text_file:
+            textFile = open(args.text_file, 'r')
+            text = textFile.read()
+            textFile.close()
+        else:
+            text = sys.stdin.read()
+
+        train(text, args.model_file)
+    elif args.mode == "hack":
+        json_file = open(args.model_file, 'r')
+        default_stats = json.load(json_file)
+        json_file.close()
+        output_string = hack_caesar(input_string, default_stats)
+
+    if args.output_file:
+        output_file = open(args.output_file, 'w')
+        output_file.write(output_string)
+        output_file.close()
     else:
-        input_string = sys.stdin.read()
-
-if args.cipher == "caesar":
-    key = int(args.key)
-    if args.mode == "decode":
-        key = -key
-    output_string = caeser(input_string, key)
-if args.cipher == "vignere":
-    output_string = vignere(input_string, args.key, args.mode)
-
-if args.mode == "train":
-    text = ''
-    if args.text_file:
-        textFile = open(args.text_file, 'r')
-        text = textFile.read()
-        textFile.close()
-    else:
-        text = sys.stdin.read()
-
-    train(text, args.model_file)
-elif args.mode == "hack":
-    json_file = open(args.model_file, 'r')
-    default_stats = json.load(json_file)
-    json_file.close()
-    output_string = hack_caesar(input_string, default_stats)
-
-if args.output_file:
-    output_file = open(args.output_file, 'w')
-    output_file.write(output_string)
-    output_file.close()
-else:
-    print(output_string)
+        print(output_string)
